1113ac0f01bd2679908af5aa0f44f4bba84a3656
Tahrin Maruf <tm5682@mun.ca>
2022-10-08 21:03:52 -0400

    3rd in new git

--- routes/contact.js
+++ routes/contact.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/contact', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('contact', {page:'Contact', menuId:'contact'});
 });
 
